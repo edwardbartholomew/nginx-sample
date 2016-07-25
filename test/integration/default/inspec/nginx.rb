a62577381e75f0ef398b4b7b8eed0ca1cc38f7c9
Ed Bartholomew <edmbartholomew@gmail.com>
2016-07-25 10:58:47 -0400

    add test kitchen file and inspec test

---
+++ test/integration/default/inspec/nginx.rb
@@ -0,0 +1,27 @@
+describe package('nginx') do
+  it { should be_installed }
+end
+
+control '01' do
+  impact 0.7
+  title 'Verify nginx service'
+  desc 'Ensures nginx service is up and running'
+  describe service('nginx') do
+    it { should be_enabled }
+    it { should be_installed }
+    it { should be_running }
+  end
+end
+
+describe port(80) do
+  it { should be_listening }
+  its('processes') { should eq ['nginx:'] }
+  its('protocols') { should eq ['tcp'] }
+end
+
+web_user = 'www-data'
+web_user = 'nginx' if os[:family] == 'centos'
+
+describe user(web_user) do
+  it { should exist }
+end
